14e2b65e04dec9283217b01fd2156b7d5bfdab14
C2MX <xuming880@gmail.com>
2023-03-09 13:30:05 +0800

    Update ansysmac_beta.php

--- tools/ansysmac_beta.php
+++ tools/ansysmac_beta.php
@@ -66,11 +66,11 @@ else{//如果没有点击提交按钮，则macname参数是空值，则显示表
             </div>
             <div class="form-floating mb-3">
                 <select class="form-select" id="sel1" name="ysnum">
-                    <option value="2" selected>2</option>
+                    <option value="2">2</option>
                     <option value="3">3</option>
                     <option value="4">4</option>
                     <option value="5">5</option>
-                    <option value="6">6</option>
+                    <option value="6" selected>6</option>
                     <option value="7">7</option>
                     <option value="8">8</option>
                 </select>
