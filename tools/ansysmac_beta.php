b767406668f1e7aed931d93311b88607801d0707
C2MX <xuming880@gmail.com>
2023-03-09 12:56:41 +0800

    Update ansysmac_beta.php

--- tools/ansysmac_beta.php
+++ tools/ansysmac_beta.php
@@ -14,7 +14,7 @@ $macname = strval(isset($_POST['macname'])? $_POST['macname'] : '');//宏文件�
 if($macname){
 }else{
 ?>
-<script src="/js/ansysmac_beta.js"></script>
+    <script src="/js/ansysmac_beta.js"></script>
     <?php
 }
 ?>
@@ -46,25 +46,25 @@ else{//如果没有点击提交按钮，则macname参数是空值，则显示表
 	//form表单提交数据是name标签
 ?>
         <form action="" method="post">
-            <div class="form-floating">
+            <div class="form-floating mb-3">
                 <input type="text" class="form-control" id="macname" name="macname" placeholder="输入宏名称">
                 <label for="macname">宏名称</label>
             </div>
-            <div class="row mb-3 mt-3">
+            <div class="row mb-3">
                 <div class="col-md-2">
                     <div class="form-check">
-                        <input name="loadkpnum" class="form-check-input" type="radio" id="load1" value="1" checked>
+                        <input name="loadkpnum" class="form-check-input" type="radio" id="load1" value="1">
                         <label class="form-check-label" for="load1">1点加载</label>
                     </div>
                 </div>
                 <div class="col-md-2">
                     <div class="form-check">
-                        <input name="loadkpnum" class="form-check-input" type="radio" id="load4" value="4">
+                        <input name="loadkpnum" class="form-check-input" type="radio" id="load4" value="4" checked>
                         <label class="form-check-label" for="load4">4点加载</label>
                     </div>
                 </div>
             </div>
-            <div class="form-floating mb-3 mt-3">
+            <div class="form-floating mb-3">
                 <select class="form-select" id="sel1" name="ysnum">
                     <option value="2" selected>2</option>
                     <option value="3">3</option>
@@ -76,7 +76,7 @@ else{//如果没有点击提交按钮，则macname参数是空值，则显示表
                 </select>
                 <label for="sel1" class="form-label">约束点数量</label>
             </div>
-            <div class="row mb-3 mt-3">
+            <div class="row mb-3">
                 <div class="col-md-2">
                     <div class="form-check">
                         <input name="casenum" class="form-check-input" type="radio" id="case8" value="8" checked>
@@ -92,73 +92,78 @@ else{//如果没有点击提交按钮，则macname参数是空值，则显示表
             </div>
             <div class="input-group mb-3">
                 <span class="input-group-text">加载点编号:</span>
-                <input type="text" class="form-control" name="kp1" value="1">
-                <input type="text" class="form-control" name="kp2" value="2">
-                <input type="text" class="form-control" name="kp3" value="3">
-                <input type="text" class="form-control" name="kp4" value="4">
+                <input type="text" class="form-control" name="kp1">
+                <input type="text" class="form-control" name="kp2">
+                <input type="text" class="form-control" name="kp3">
+                <input type="text" class="form-control" name="kp4">
             </div>
 
             <div class="input-group mb-3">
                 <span class="input-group-text">约束点编号:</span>
-                <input type="text" class="form-control" name="node1" value="1">
-                <input type="text" class="form-control" name="node2" value="2">
-                <input type="text" class="form-control" name="node3" value="3">
-                <input type="text" class="form-control" name="node4" value="4">
-                <input type="text" class="form-control" name="node5" value="5">
-                <input type="text" class="form-control" name="node6" value="6">
-                <input type="text" class="form-control" name="node7" value="7">
-                <input type="text" class="form-control" name="node8" value="8">
+                <input type="text" class="form-control" name="node1">
+                <input type="text" class="form-control" name="node2">
+                <input type="text" class="form-control" name="node3">
+                <input type="text" class="form-control" name="node4">
+                <input type="text" class="form-control" name="node5">
+                <input type="text" class="form-control" name="node6">
+                <input type="text" class="form-control" name="node7">
+                <input type="text" class="form-control" name="node8">
             </div>
 
             <div class="form-check form-switch mb-3">
                 <input class="form-check-input" type="checkbox" id="fix" name="fix" value="1">
-                <label class="form-check-label" for="fix">节点是否约束了旋转</label>
+                <label class="form-check-label text-nowrap" for="fix">节点是否约束了旋转</label>
             </div>
 
             <div class="input-group mb-3">
                 <span class="input-group-text">垂直力</span>
-                <input type="text" class="form-control" placeholder="0" name="fv">
+                <input type="text" class="form-control" placeholder="" name="fv">
                 <span class="input-group-text">t</span>
             </div>
             <div class="input-group mb-3">
                 <span class="input-group-text">水平力</span>
-                <input type="text" class="form-control" placeholder="0" name="fs">
+                <input type="text" class="form-control" placeholder="" name="fs">
                 <span class="input-group-text">t</span>
             </div>
             <div class="input-group mb-3">
                 <span class="input-group-text">弯矩</span>
-                <input type="text" class="form-control" placeholder="0" name="m">
+                <input type="text" class="form-control" placeholder="" name="m">
                 <span class="input-group-text">t.m</span>
             </div>
             <div class="input-group mb-3">
                 <span class="input-group-text">扭矩</span>
-                <input type="text" class="form-control" placeholder="0" name="mk">
+                <input type="text" class="form-control" placeholder="" name="mk">
                 <span class="input-group-text">t.m</span>
             </div>
             <div class="input-group mb-3">
                 <span class="input-group-text">扭矩力</span>
-                <input type="text" class="form-control" placeholder="0" name="fmk">
+                <input type="text" class="form-control" placeholder="" name="fmk">
                 <span class="input-group-text">t</span>
             </div>
-
-            <div class="form-check form-switch mb-3">
-                <input class="form-check-input" type="checkbox" id="rf" name="rf" value="1" checked>
-                <label class="form-check-label" for="rf">提取节点反力</label>
+            <div class="row">
+                <div class="col-md">
+                    <div class="form-check form-switch mb-3">
+                        <input class="form-check-input" type="checkbox" id="rf" name="rf" value="1" checked>
+                        <label class="form-check-label text-nowrap" for="rf">提取节点反力</label>
+                    </div>
+                </div>
+                <div class="col-md">
+                    <div class="form-check form-switch mb-3">
+                        <input class="form-check-input" type="checkbox" id="rm" name="rm" value="1">
+                        <label class="form-check-label text-nowrap" for="rm">提取节点反弯矩</label>
+                    </div>
+                </div>
+                <div class="col-md">
+                    <div class="form-check form-switch mb-3">
+                        <input class="form-check-input" type="checkbox" id="af" name="af" value="1">
+                        <label class="form-check-label text-nowrap" for="af">提取杆件轴力</label>
+                    </div>
+                </div>
             </div>
-
-            <div class="form-check form-switch mb-3">
-                <input class="form-check-input" type="checkbox" id="rm" name="rm" value="1">
-                <label class="form-check-label" for="rm">提取节点反弯矩</label>
-            </div>
-
-            <div class="form-check form-switch mb-3">
-                <input class="form-check-input" type="checkbox" id="af" name="af" value="1">
-                <label class="form-check-label" for="af">提取杆件轴力</label>
-            </div>
-
             <div class="form-floating mb-3">
                 <select class="form-select" id="sel2" name="afnum">
-                    <option value="1" selected>1</option>
+                    <option value="0" selected>0</option>
+                    <option value="1">1</option>
                     <option value="2">2</option>
                     <option value="3">3</option>
                     <option value="4">4</option>
@@ -172,44 +177,44 @@ else{//如果没有点击提交按钮，则macname参数是空值，则显示表
 
             <div class="input-group mb-3">
                 <span class="input-group-text">杆件编号:</span>
-                <input type="text" class="form-control" name="elem1" value="1">
-                <input type="text" class="form-control" name="elem2" value="2">
-                <input type="text" class="form-control" name="elem3" value="3">
-                <input type="text" class="form-control" name="elem4" value="4">
-                <input type="text" class="form-control" name="elem5" value="5">
-                <input type="text" class="form-control" name="elem6" value="6">
-                <input type="text" class="form-control" name="elem7" value="7">
-                <input type="text" class="form-control" name="elem8" value="8">
+                <input type="text" class="form-control" name="elem1">
+                <input type="text" class="form-control" name="elem2">
+                <input type="text" class="form-control" name="elem3">
+                <input type="text" class="form-control" name="elem4">
+                <input type="text" class="form-control" name="elem5">
+                <input type="text" class="form-control" name="elem6">
+                <input type="text" class="form-control" name="elem7">
+                <input type="text" class="form-control" name="elem8">
             </div>
             <div class="row">
                 <div class="col-md">
                     <div class="form-check form-switch mb-3">
                         <input class="form-check-input" type="checkbox" id="dfx" name="dfx" value="1">
-                        <label class="form-check-label" for="dfx">输出整体X向位移云图</label>
+                        <label class="form-check-label text-nowrap" for="dfx">输出整体X向位移云图</label>
                     </div>
                 </div>
                 <div class="col-md">
                     <div class="form-check form-switch mb-3">
                         <input class="form-check-input" type="checkbox" id="dfy" name="dfy" value="1">
-                        <label class="form-check-label" for="dfy">输出整体y向位移云图</label>
+                        <label class="form-check-label text-nowrap" for="dfy">输出整体y向位移云图</label>
                     </div>
                 </div>
                 <div class="col-md">
                     <div class="form-check form-switch mb-3">
                         <input class="form-check-input" type="checkbox" id="dfz" name="dfz" value="1">
-                        <label class="form-check-label" for="dfz">输出整体z向位移云图</label>
+                        <label class="form-check-label text-nowrap" for="dfz">输出整体z向位移云图</label>
                     </div>
                 </div>
                 <div class="col-md">
                     <div class="form-check form-switch mb-3">
                         <input class="form-check-input" type="checkbox" id="dftotal" name="dftotal" value="1">
-                        <label class="form-check-label" for="dftotal">输出整体合位移云图</label>
+                        <label class="form-check-label text-nowrap" for="dftotal">输出整体合位移云图</label>
                     </div>
                 </div>
                 <div class="col-md">
                     <div class="form-check form-switch mb-3">
                         <input class="form-check-input" type="checkbox" id="stotal" name="stotal" value="1">
-                        <label class="form-check-label" for="stotal">输出整体合应力云图</label>
+                        <label class="form-check-label text-nowrap" for="stotal">输出整体合应力云图</label>
                     </div>
                 </div>
             </div>
@@ -233,38 +238,39 @@ else{//如果没有点击提交按钮，则macname参数是空值，则显示表
                 <div class="col-md">
                     <div class="form-check form-switch mb-3">
                         <input class="form-check-input" type="checkbox" id="cpnx" name="cpnx" value="1">
-                        <label class="form-check-label" for="cpnx">输出组件CPN X向位移</label>
+                        <label class="form-check-label text-nowrap" for="cpnx">输出组件CPN X向位移</label>
                     </div>
                 </div>
                 <div class="col-md">
                     <div class="form-check form-switch mb-3">
                         <input class="form-check-input" type="checkbox" id="cpny" name="cpny" value="1">
-                        <label class="form-check-label" for="cpny">输出组件CPN y向位移</label>
+                        <label class="form-check-label text-nowrap" for="cpny">输出组件CPN y向位移</label>
                     </div>
                 </div>
                 <div class="col-md">
                     <div class="form-check form-switch mb-3">
                         <input class="form-check-input" type="checkbox" id="cpnz" name="cpnz" value="1">
-                        <label class="form-check-label" for="cpnz">输出组件CPN z向位移</label>
+                        <label class="form-check-label text-nowrap" for="cpnz">输出组件CPN z向位移</label>
                     </div>
                 </div>
                 <div class="col-md">
                     <div class="form-check form-switch mb-3">
                         <input class="form-check-input" type="checkbox" id="cpnt" name="cpnt" value="1">
-                        <label class="form-check-label" for="cpnt">输出组件CPN 合位移</label>
+                        <label class="form-check-label text-nowrap" for="cpnt">输出组件CPN 合位移</label>
                     </div>
                 </div>
                 <div class="col-md">
                     <div class="form-check form-switch mb-3">
                         <input class="form-check-input" type="checkbox" id="cpnst" name="cpnst" value="1">
-                        <label class="form-check-label" for="cpnst">输出组件CPN 合应力</label>
+                        <label class="form-check-label text-nowrap" for="cpnst">输出组件CPN 合应力</label>
                     </div>
                 </div>
             </div>
 
             <div class="row mb-3">
                 <div class="d-grid col mx-auto">
-                    <button type="reset" class="btn btn-danger btn-lg text-nowrap">重置页面</button>
+                    <button type="button" class="btn btn-danger btn-lg text-nowrap"
+                        onclick="javascript:location.reload()">重置页面</button>
                 </div>
                 <div class="d-grid col mx-auto">
                     <button type="submit" class="btn btn-primary btn-lg text-nowrap">生成脚本</button>
